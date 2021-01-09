cc199e62899639356c713628e1344ddf25cbd7e2
gusals005 <gusals005@gmail.com>
2021-01-09 15:26:22 +0000

    merge

--- app.js
+++ app.js
@@ -4,33 +4,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-
-/**for save image
-var multer, storage, crypto;
-multer = require('multer');
-crypto = require('crypto');
-
-var form = "<!DOCTYPE HTML><html><body>" +
-"<form method='post' action='/upload' enctype='multipart/form-data'>" +
-"<input type='file' name='upload'/>" +
-"<input type='submit' /></form>" +
-"</body></html>";
-
-var fs = require('fs');
-
-storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function(req, file, cb) {
-    return crypto.pseudoRandomBytes(16, function(err, raw) {
-      if (err) {
-        return cb(err);
-      }
-      return cb(null, "" + (raw.toString('hex')) + (path.extname(file.originalname)));
-    });
-  }
-});
-*/
-
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -38,8 +11,9 @@ var logger = require('morgan');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var contactRouter = require('./routes/contact');
+var photoRouter = require('./routes/photo');
 
 var PORT = 8080;
 
@@ -57,41 +31,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 //var Book = require('./models/book');
 //var router = require('./routes/index')(app, Book);
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-//connect to mongodb server
-var mongoClient = require('mongodb').MongoClient;
-var db_name = 'madcamp_project2';
-var mydb = null;
-
-
-/*mongoClient.connect('mongodb://localhost/' + db_name, function(error, client){
-  if(error){
-    console.log(error);
-  }else{
-    console.log("connected: " + db_name);
-    mydb = client.db(db_name);
-    client.close();
-    
-    var cursor = mydb.collection('contacts').find();
-    cursor.each(function(err,doc){
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(doc != null){
-                console.log(doc);
-            }
-            else{
-              console.log("으악");
-            }
-        }
-    });
-    //app.set('mydb', mydb);
-    
-  }
-})*/
+app.use('/login',loginRouter);
+app.use('/contacts',contactRouter);
+app.use('/photos',photoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -113,6 +56,6 @@ var server = app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
 
-module.exports = {app, mydb,};
+module.exports = app;
 //module.exports = mydb;
 
