79edc62730e3b5e0cfe29deefefba26ac6723262
gusals005 <gusals005@gmail.com>
2021-01-09 14:49:15 +0000

    get('/photos')

--- routes/index.js
+++ routes/index.js
@@ -112,8 +112,43 @@ var form = "<!DOCTYPE HTML><html><body>" +
 */
 
 router.get('/photos', function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/html' });
-    res.end(form);
+    //res.writeHead(200, {'Content-Type': 'text/html' });
+    //res.end(form);
+
+    collection_name = 'photos'        
+
+    mongoClient.connect('mongodb://localhost/', function(error, client){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("connected: " + db_name);
+            mydb = client.db(db_name);
+
+            collection = mydb.collection(collection_name);
+            collection.find({}).toArray(function(err, results){
+                res.status(200).json({'Photos' : results});
+              });
+
+            //////////// For DEBUG //////
+            var cursor = mydb.collection(collection_name).find();
+            cursor.each(function (err, doc) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    if (doc != null) {
+                        console.log(doc);
+                    }
+                    else {
+                        console.log("으악");
+                    }
+                }
+
+            });
+            /////////////////////////////
+
+        } 
+    });
   
 });
 // Post files
